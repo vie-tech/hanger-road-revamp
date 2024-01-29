592f94e9570a6fa64ea652eb90f5bb5be533ff3c
Ativie favour <ativiefavour03@gmail.com>
2024-01-29 18:54:47 +0100

    tweaks

--- src/components/home/featured/FeaturedCard.jsx
+++ src/components/home/featured/FeaturedCard.jsx
@@ -6,37 +6,44 @@ import {motion} from "framer-motion"
 const FeaturedCard = () => {
   return (
     <>
-      <div className='content grid5 mtop'>
+      <div className="content grid5 mtop">
         {featured.map((items, index) => (
-          <Link to={`${items.link}`}>
-           {items.left ? (<motion.div
-            className="box"
-            initial={{opacity: 0, x: -100}}
-            whileInView={{opacity: 1, x: 0}}
-            transition={{
-              duration: 1
-            }}
-            viewport={{once: true}}>
-           <img src={items.cover} alt='' />
-           <h4>{items.name}</h4>
-           </motion.div>) : (<motion.div
-            className="box"
-            initial={{opacity: 0, x: 100}}
-            whileInView={{opacity: 1, x: 0}}
-            transition={{
-              duration: 1
-            }}
-            viewport={{once: true}}
-            >
-           <img src={items.cover} alt='' />
-           <h4>{items.name}</h4>
-           </motion.div>)}
+          <Link
+            to={`${items.link}`}
+            style={{ textDecoration: "none", color: "#2d3954" }}
+          >
+            {items.left ? (
+              <motion.div
+                className="box"
+                initial={{ opacity: 0, x: -100 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                transition={{
+                  duration: 1,
+                }}
+                viewport={{ once: true }}
+              >
+                <img src={items.cover} alt="" />
+                <h4>{items.name}</h4>
+              </motion.div>
+            ) : (
+              <motion.div
+                className="box"
+                initial={{ opacity: 0, x: 100 }}
+                whileInView={{ opacity: 1, x: 0 }}
+                transition={{
+                  duration: 1,
+                }}
+                viewport={{ once: true }}
+              >
+                <img src={items.cover} alt="" />
+                <h4>{items.name}</h4>
+              </motion.div>
+            )}
           </Link>
         ))}
       </div>
     </>
-   
-  )
+  );
 }
 
 export default FeaturedCard
